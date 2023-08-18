212a886f8b2ce47964f5cd058e3066b835f3dada
Mohamed Yusuf <Mohamed-yusuf2@hotmail.com>
2023-08-17 20:58:02 -0500

    create database and updated pool module

--- database.sql
+++ database.sql
@@ -8,3 +8,18 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "plan" (
+    "id" SERIAL PRIMARY KEY,
+    "task" VARCHAR (255) NOT NULL,
+    "comments" VARCHAR (4000),
+    "isComplete" BOOLEAN NOT NULL DEFAULT false,
+    "user_id" INTEGER REFERENCES "user" ("id")
+);
+
+CREATE TABLE "location" (
+    "id" SERIAL PRIMARY KEY,
+    "location" VARCHAR (255) NOT NULL,
+    "date_time" TIMESTAMP NOT NULL,
+    "plan_id" INTEGER REFERENCES "plan" ("id")
+);
