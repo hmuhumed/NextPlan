e562ebd557a794e6880356fdb8d855add962f13f
Hamza Muhumed <hamzamuhumed2107@gmail.com>
2023-08-18 22:32:01 -0500

    Made the queryText for our POST route to post into the database

--- server/routes/plan.router.js
+++ server/routes/plan.router.js
@@ -3,7 +3,13 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    const queryText = `INSERT INTO "plan" ("task", "comments") VALUES ($1, $2);`;
+    const queryText = `INSERT INTO "plan" ("task", "comments", "user_id") VALUES ($1, $2, $3);`;
+    pool.query(queryText, [req.body.task, req.body.comments, req.user.id])
+    .then(results => {
+        res.sendStatus(201);
+    }).catch(err => {
+        console.log("Error in POST /plan.router", err);
+    })
 })
 
 module.exports = router;
