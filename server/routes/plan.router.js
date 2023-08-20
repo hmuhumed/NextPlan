0a0d455d0a6535ba5119a1367d127ada7d304e1c
Hamza Muhumed <hamzamuhumed2107@gmail.com>
2023-08-19 20:41:42 -0500

    Added code comments to the POST route

--- server/routes/plan.router.js
+++ server/routes/plan.router.js
@@ -3,11 +3,18 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.post('/', (req, res) => {
+
+    // Constructing a SQL query to insert a new plan item into the "plan" table.
+    // The query includes placeholders ($1, $2, $3) for parameterized values to prevent SQL injection.
     const queryText = `INSERT INTO "plan" ("task", "comments", "user_id") VALUES ($1, $2, $3);`;
+    // Executing an SQL query to insert a new plan item into the "plan" table.
+    // The query includes parameterized values taken from the request body and the authenticated user's ID.
     pool.query(queryText, [req.body.task, req.body.comments, req.user.id])
+    // If the query is successful, respond with a 201 status code.
     .then(results => {
         res.sendStatus(201);
     }).catch(err => {
+         // If an error occurs during the query execution, log the error for debugging purposes.
         console.log("Error in POST /plan.router", err);
     })
 })
@@ -24,7 +31,6 @@ pool.query(sqlText,[req.user.id])
      .then((result) =>{
       console.log('upcoming plans:',result.rows)
       res.send(result.rows)
-      
      })
      .catch((error) =>{
       console.log('error fetching plans', error)
@@ -33,7 +39,7 @@ pool.query(sqlText,[req.user.id])
   });
 
 router.put('/:id', (req, res) => {
-    const idToUpdate = req.params.id
+    const idToUpdate = req.params.id;
     const queryText = `UPDATE "plan" SET "isComplete" = true WHERE user_id = $1;`; 
     pool.query(queryText, [idToUpdate])
     .then(result => {
