4c140853c146bd4aafd21c73ef4c8978784ab3f0
Mohamed Yusuf <Mohamed-yusuf2@hotmail.com>
2023-08-24 10:35:55 -0500

    create env file to store api key

--- server/routes/plan.router.js
+++ server/routes/plan.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+require('dotenv').config();
 
 router.post('/', (req, res) => {
     const queryText = `INSERT INTO "plan" ("task", "location", "date_time","comments","user_id") VALUES 
